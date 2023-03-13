917b775b284ec8f77e27abc56749e8fc3621d666
thel3onardo <leonardo.contato03@gmail.com>
2023-03-13 18:38:57 -0300

    feat: set projects design system colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#3DA9FC',
+				dark: '#094067',
+				light: '#D8EEFE',
+				gray: '#606C7A'
+			},
+			fontFamily: {
+				'montserrat': 'Montserrat'
+			}
+		},
 	},
 	plugins: [],
 }
