b0d515084437ac38a7eddecca08fc805aa696810
thel3onardo <leonardo.contato03@gmail.com>
2023-03-13 17:45:15 -0300

    feat: setup astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
@@ -6,3 +7,9 @@ import tailwind from '@astrojs/tailwind'
 export default defineConfig({
   integrations: [tailwind()],
 })
+=======
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({});
+>>>>>>> 4992857 (feat: setup astro)
