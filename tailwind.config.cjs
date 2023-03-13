6436019b806a7ffbc0aa898a83d6b71a45d90aac
thel3onardo <leonardo.contato03@gmail.com>
2023-03-13 20:07:54 -0300

    feat: setup and configure eslint and prettier

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3DA9FC',
+        dark: '#094067',
+        light: '#D8EEFE',
+        gray: '#606C7A',
+      },
+      fontFamily: {
+        montserrat: 'Montserrat',
+      },
+    },
+  },
+  plugins: [],
 }
