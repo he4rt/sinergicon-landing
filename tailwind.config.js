e1f88eb47c8628eb9c18c54f994fb0b415991608
thel3onardo <leonardo.contato03@gmail.com>
2023-03-13 20:22:06 -0300

    feat: install and setup eslint-plugin-tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3DA9FC',
+        dark: '#094067',
+        light: '#D8EEFE',
+        gray: '#606C7A',
+      },
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
