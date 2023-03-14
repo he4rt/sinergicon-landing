b0aaf119869ab98ec305261a1d62710b286b0cee
thel3onardo <leonardo.contato03@gmail.com>
2023-03-14 08:31:31 -0300

    feat: improve serviceItem component

--- src/data/index.ts
+++ src/data/index.ts
@@ -3,30 +3,30 @@ export const ourServicesList = [
     title: 'Contabilidade com foco em gestão',
     content:
       'Escrituração contábil com foco na praticidade, tanto no input quanto no outout das informações. Muito mais do que cumprir obrigações burocráticas, nosso objetivo é preparar todas as demonstrações contábeis',
-    color: '#3DA9FC',
+    rgbColor: '61, 169, 252',
   },
   {
     title: 'Contabilidade tributária',
     content:
       'Apuração mensal dos tributos referentes a atividade e regime tributário da empresa, bem como escrituração de todas as movimentações de entradas, saídas, serviços prestados e tomados. Gestão e entrega das obrigações acessórias frente a Receita Federal',
-    color: '#FC823D',
+    rgbColor: '252, 130, 61',
   },
   {
     title: 'Trabalhista',
     content:
       'Execução de rotinas de funcionários, como: admissão, demissão, afastamento, licença maternidade, entre outros. Cálculo dos encargos tributários mensais de folha de pagamento, bem como encargos referente aos empregados (salário, décimo terceiro, férias, etc.). ',
-    color: '#EF4565',
+    rgbColor: '239, 69, 101',
   },
   {
     title: 'Planejamento Financeiro',
     content:
       'A proposta da Sinergicon é aliar a contabilidade com a gestão financeira, deste modo, buscamos trabalhar de maneira que possamos oferecer ao cliente a sensação de estar no controle de suas informações como um todo, demonstrando os diversos dados.',
-    color: '#3BE876',
+    rgbColor: '59, 232, 118',
   },
   {
     title: 'Gestão e controladoria',
     content:
       'Suportado dos números contábeis e financeiros e na busca constante em estar mais próximo ao cliente, trabalhamos com a contabilidade consultiva que busca auxiliar os gestores no processo decisório.',
-    color: '#913DFC',
+    rgbColor: '145, 61, 252',
   },
 ]
